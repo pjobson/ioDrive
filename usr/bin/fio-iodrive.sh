53c2c81a4c4e9e265aade720448b168f640b86a7
Paul Jobson <pjobson@gmail.com>
2023-10-28 00:30:10 -0400

    completed install

--- usr/bin/fio-iodrive.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-DEV_LETTER=$(echo $1 | sed 's/^.*fio//')
-DEV_NUMBER=$(echo ${DEV_LETTER} | tr a-z 0-9)
-FIO_SERIAL=$(grep SN: /proc/fusion/fct${DEV_NUMBER}/iodrive/board_info | sed 's/^.*SN: *//')
-echo "${FIO_SERIAL}"
